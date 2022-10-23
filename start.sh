f9c50e7e63d414514ce502150e14a439650309b3
SupahFox <SupahFox@Github.com>
2022-10-23 04:26:47 +0000

    menú

--- start.sh
+++ start.sh
@@ -10,35 +10,48 @@ echo "Ingresa una opción
 	2) ABM de grupo
 	3) Test de conexión a internet
 	4) Actualizar sistema
-	5) Salir del programa"
+	5) Comprobar existencia de usuarios en la base de datos
+	6) Administrar servicios
+	7) Salir del programa"
+
 read -p "Ingresa una opción: " op
 
 	if [ $op -eq 1 ]
 	then
-	continuar=2
-	./ABMu.sh
+		continuar=2
+		./ABMu.sh
 	fi
 
 	if [ $op -eq 2 ]
 	then
-	continuar=2
-	./ABMg.sh
+		continuar=2
+		./ABMg.sh
 	fi
 
 	if [ $op -eq 3 ]
 	then
-	./testinternet.sh
+		./testinternet.sh
 	fi
 
 	if [ $op -eq 4 ]
 	then
-	./apt.sh
+		./apt.sh
 	fi
 
 	if [ $op -eq 5 ]
 	then
-	clear
-	exit 0
+		./dbusuarios.sh
+	fi
+
+	if [ $op -eq 6 ]
+	then
+		./servicios.sh
+	fi
+
+	if [ $op -eq 7 ]
+	then
+		clear
+		exit 0
 	fi
 clear
 done
