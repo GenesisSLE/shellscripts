db3ed8a65f1fc92a93cbd6151e577ed5cf27273b
SupahFox <SupahFox@Github.com>
2022-10-27 01:23:39 +0000

    consultas faltantes (falta la de los feriados)

--- inasistencias_profesor.sh
+++ inasistencias_profesor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-sudo mysql -u root -e "select sum(if(claseDic = 0, 1, 0)) as ClasesNoDictadas, G.orientacion as Orientacion, G.grado as GradoGrupo, A.nombre as NombreAsignaura from Dicta D join Clase C on C.idClase = D.idClase join Tiene2 T on T.idClase = C.idClase join Cursa Cu on T.idCursa = Cu.idCursa join Asignatura A on A.idMat = Cu.idMat join Grupo G on G.idGrupo = Cu.idGrupo;"
+sudo mysql -u root -e "USE genesis_sle; select sum(if(claseDic = 0, 1, 0)) as ClasesNoDictadas, G.orientacion as Orientacion, G.grado as GradoGrupo, A.nombre as NombreAsignaura from Dicta D join Clase C on C.idClase = D.idClase join Tiene2 T on T.idClase = C.idClase join Cursa Cu on T.idCursa = Cu.idCursa join Asignatura A on A.idMat = Cu.idMat join Grupo G on G.idGrupo = Cu.idGrupo;"
